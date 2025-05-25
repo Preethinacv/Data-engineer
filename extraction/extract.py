f5142ce7eaf8b520d0cf762a1a6468fa652f17ec
Preethina <127195190+Preethinacv@users.noreply.github.com>
2025-05-25 17:48:25 +0530

    Update extract.py

--- extraction/extract.py
+++ extraction/extract.py
@@ -1,16 +1,5 @@
 import json
 from googleapiclient.discovery import build
-
-# def fetch_videos(api_key, channel_id, max_results=10):
-#     youtube = build("youtube", "v3", developerKey=api_key)
-#     request = youtube.search().list(
-#         part="snippet",
-#         channelId=channel_id,
-#         maxResults=max_results,
-#         order="date"
-#     )
-#     response = request.execute()
-#     return response['items']
 def fetch_videos(api_key, channel_id):
     youtube = build("youtube", "v3", developerKey=api_key)
     request = youtube.search().list(
@@ -20,8 +9,5 @@ def fetch_videos(api_key, channel_id):
         order="date"
     )
     response = request.execute()
-    print("✅ API call successful. Videos fetched.")
     videos =response.get("items", [])
-    # return response.get("items", [])
-    print(f"✅ API call successful. Videos fetched: {len(videos)} items.")
     return videos
